189c067196afde91cacc885f1100c7e9656f9c3e
pandekar <pandeadhistanaya@gmail.com>
2024-04-16 14:52:54 +0700

    break queries into separate files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 const Images = async () => {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { asc }) => asc(model.id)
-  })
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
---
+++ src/server/queries.ts
@@ -0,0 +1,18 @@
+import 'server-only'
+import { db } from './db'
+import { auth } from '@clerk/nextjs/server'
+
+const getMyImages = async () => {
+  const user = auth();
+
+  if (!user.userId) throw new Error('Unauthorized');
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { asc }) => asc(model.id)
+  })
+
+  return images
+}
+
+export {getMyImages}
